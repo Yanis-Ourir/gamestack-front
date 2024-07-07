92cd8cf2bf0db01d79457369d5bd8922ee18d4e5
Yanis <ourir.yanis@gmail.com>
2024-07-07 19:20:33 +0200

    landing page

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
             <Image src="/assets/place_holder_logo.png" alt="Logo GameStack" width={100} height={50}/>
             <p>GameStack</p>
         </div>
-        <ul className="flex mr-6">
+        <ul className="flex mr-6 text-3xl">
             <li><a className="px-4 hover:text-[#B54548] active:text-[#B54548]" href="/home">Home</a></li>
             <li><a className="px-4 hover:text-[#B54548] active:text-[#B54548]" href="/about">About</a></li>
             <li><a className="px-4 hover:text-[#B54548] active:text-[#B54548]" href="/contact">Contact</a></li>
